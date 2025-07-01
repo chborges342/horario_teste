975282785f7d16c2698b86170d836488223cef7a
Carlos Henrique Leite Borges <67389261+chborges342@users.noreply.github.com>
2025-06-30 23:03:47 -0300

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,24 +1,25 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
 import { 
-  getFirestore,
-  doc,
+  getFirestore, 
+  collection, 
+  doc, 
+  setDoc, 
   getDoc,
-  setDoc,
-  onSnapshot,
-  collection  // Adicionei collection que pode ser útil
-} from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
+  onSnapshot
+} from "https://www.gstatic.com/firebasejs/11.9.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAXbpZb2J79z-jIGP4lQPgyZ9dG5Z6FkS0",
-  authDomain: "sistema-horarios.firebaseapp.com",
-  projectId: "sistema-horarios",
-  storageBucket: "sistema-horarios.firebasestorage.app",
-  messagingSenderId: "799603590846",
-  appId: "1:799603590846:web:04e0222c93387b8e3180ef"
+  apiKey: "AIzaSyAppeJPEhGPSxqPsyeaWmA-i3mMJNS0SFI",
+  authDomain: "gestao-horarios-62d90.firebaseapp.com",
+  projectId: "gestao-horarios-62d90",
+  storageBucket: "gestao-horarios-62d90.firebasestorage.app",
+  messagingSenderId: "178826790802",
+  appId: "1:178826790802:web:c2553cbda489c610cb234b"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-// Exporte todas as funções que você vai usar
-export { db, collection, doc, getDoc, setDoc, onSnapshot };
+export { db, collection, doc, setDoc, getDoc, onSnapshot };
