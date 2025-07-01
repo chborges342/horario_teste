c711508dbacf7562600271a7a07416cd4090f744
Carlos Henrique Leite Borges <67389261+chborges342@users.noreply.github.com>
2025-06-30 22:03:22 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,25 +1,24 @@
-// firebase.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
 import { 
-  getFirestore, 
-  collection, 
-  doc, 
-  setDoc, 
+  getFirestore,
+  collection,
+  doc,
+  setDoc,
   getDoc,
   onSnapshot
-} from "https://www.gstatic.com/firebasejs/11.9.1/firebase-firestore.js";
+} from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAppeJPEhGPSxqPsyeaWmA-i3mMJNS0SFI",
-  authDomain: "gestao-horarios-62d90.firebaseapp.com",
-  projectId: "gestao-horarios-62d90",
-  storageBucket: "gestao-horarios-62d90.firebasestorage.app",
-  messagingSenderId: "178826790802",
-  appId: "1:178826790802:web:c2553cbda489c610cb234b"
+  apiKey: "AIzaSyAXbpZb2J79z-jIGP4lQPgyZ9dG5Z6FkS0",
+  authDomain: "sistema-horarios.firebaseapp.com",
+  projectId: "sistema-horarios",
+  storageBucket: "sistema-horarios.firebasestorage.app",
+  messagingSenderId: "799603590846",
+  appId: "1:799603590846:web:04e0222c93387b8e3180ef"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-export { db, collection, doc, setDoc, getDoc, onSnapshot };
+export { db, doc, setDoc, getDoc, onSnapshot };
